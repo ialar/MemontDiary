643028f21ce6f0739947d0c9b20a60addd84299d
Igor Larionov <jedialar@gmail.com>
2024-11-22 15:03:26 +0300

    edit admin.py and create superuser

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import Entry
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'created_at')
+    list_filter = ('title', 'created_at')
+    search_fields = ('title', 'text')
