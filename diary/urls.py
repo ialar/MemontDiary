ec9ab54b99700ccdf7831bda52ce7e33cd1bb6bf
Igor Larionov <jedialar@gmail.com>
2024-11-28 22:01:50 +0300

    add urls and CRUD for entries of diary in views.py

--- diary/urls.py
+++ diary/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from diary.apps import DiaryConfig
-from diary.views import index
+from diary.views import Index, EntryListView, EntryCreateView, EntryUpdateView, EntryDetailView, EntryDeleteView
 
 app_name = DiaryConfig.name
 
 urlpatterns = [
-    path('', index, name='index')
+    path("", Index.as_view(), name='index'),
+    path('entries/', EntryListView.as_view(), name='entry_list'),
+    path('entry/create/', EntryCreateView.as_view(), name='entry_create'),
+    path('entry/update/<int:pk>/', EntryUpdateView.as_view(), name='entry_update'),
+    path('entry/detail/<int:pk>/', EntryDetailView.as_view(), name='entry_detail'),
+    path('entry/<int:pk>/', EntryDeleteView.as_view(), name='entry_delete'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView, DeleteView
+
+from diary.forms import EntryForm
+from diary.models import Entry
+from diary.services import get_current_date
 
 
-def index(request):
-    return render(request, 'index.html')
+class Index(TemplateView):
+    model = Entry
+    template_name = 'diary/index.html'
+    # extra_context = {'title': 'Memont diary'}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['formatted_current_date'] = get_current_date()
+        return context
+
+
+class EntryListView(ListView):
+    model = Entry
+    # extra_context = {'title': 'List of entries'}
+
+
+class EntryCreateView(CreateView):
+    model = Entry
+    form_class = EntryForm
+    success_url = reverse_lazy('diary:entry_list')
+    extra_context = {'title': 'Create entry'}
+
+
+class EntryDetailView(DetailView):
+    model = Entry
+
+
+class EntryUpdateView(UpdateView):
+    model = Entry
+    form_class = EntryForm
+    success_url = reverse_lazy('diary:entry_list')
+    extra_context = {'title': 'Edit entry'}
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    success_url = reverse_lazy('diary:entry_list')
