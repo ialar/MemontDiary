98e51aa196fe3e1eaca932ea408241cfb1c121d1
Igor Larionov <jedialar@gmail.com>
2024-12-02 00:43:27 +0300

    edit model User

--- users/models.py
+++ users/models.py
@@ -1,5 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from diary.models import NULLABLE
 
 
 class User(AbstractUser):
-    ...
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    avatar = models.ImageField(upload_to='user_pictures/', verbose_name='Avatar', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        ordering = ('email',)
