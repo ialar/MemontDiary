328ec90cd567f22696b93a3f1582639c4ca05888
Igor Larionov <jedialar@gmail.com>
2024-12-02 00:46:10 +0300

    add csu and edit admin.py in users

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,11 @@
-# from django.contrib import admin
-#
-# from users.models import User
-#
-# admin.site.register(User)
+from django.contrib import admin
+
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'avatar',
+                    'is_staff', 'is_active', 'is_superuser', 'last_login')
+    list_filter = ('is_staff', 'last_login')
+    search_fields = ('email', 'last_login')
