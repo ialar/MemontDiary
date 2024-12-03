cacef3a22c34c4ac30d9a97c9fe2972a4bd33759
Igor Larionov <jedialar@gmail.com>
2024-12-03 22:43:17 +0300

    edit views and add registration, verification and authentication of user including access levels

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView, DeleteView
 
@@ -8,32 +11,64 @@ from diary.models import Entry
 class Index(TemplateView):
     model = Entry
     template_name = 'diary/index.html'
-    # extra_context = {'title': 'Memont diary'}
 
 
-class EntryListView(ListView):
+class EntryListView(LoginRequiredMixin, ListView):
     model = Entry
-    # extra_context = {'title': 'List of entries'}
+    context_object_name = 'entry_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('query', '')
+        return context
 
 
-class EntryCreateView(CreateView):
+class EntryCreateView(LoginRequiredMixin, CreateView):
     model = Entry
     form_class = EntryForm
     success_url = reverse_lazy('diary:entry_list')
     extra_context = {'title': 'Create entry'}
 
+    def form_valid(self, form):
+        new_entry = form.save(commit=False)
+        new_entry.owner = self.request.user
+        new_entry.save()
+        return super().form_valid(form)
 
-class EntryDetailView(DetailView):
+
+class EntryDetailView(LoginRequiredMixin, DetailView):
     model = Entry
 
 
-class EntryUpdateView(UpdateView):
+class EntryUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Entry
     form_class = EntryForm
     success_url = reverse_lazy('diary:entry_list')
     extra_context = {'title': 'Edit entry'}
 
+    def test_func(self):
+        entry = self.get_object()
+        if self.request.user.is_staff:
+            return True
+        return self.request.user == entry.owner
 
-class EntryDeleteView(DeleteView):
+
+class EntryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Entry
     success_url = reverse_lazy('diary:entry_list')
+
+    def test_func(self):
+        entry = self.get_object()
+        if self.request.user.is_superuser:
+            return True
+        return self.request.user == entry.owner
+
+
+@login_required
+def entry_search(request):
+    query = request.GET.get('query', '')
+    if query:
+        search_list = Entry.objects.filter(title__icontains=query) | Entry.objects.filter(text__icontains=query)
+    else:
+        search_list = Entry.objects.none()
+    return render(request, 'diary/entry_list.html', {'entry_list': search_list, 'query': query})
--- users/views.py
+++ users/views.py
@@ -1,17 +1,79 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+import random
+import string
 
-from users.forms import UserRegisterForm, UserProfileForm
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, FormView, TemplateView
+
+from config.settings import EMAIL_HOST_USER
+from users.forms import UserRegisterForm, UserProfileForm, UserPasswordResetForm
 from users.models import User
 
 
 class UserCreateView(CreateView):
     model = User
     form_class = UserRegisterForm
-    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:register_message')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        code = ''.join([str(random.randint(0, 9)) for _ in range(10)])
+        user.verification_code = code
+        current_host = self.request.get_host()
+        user.save()
+
+        send_mail('Регистрация на сайте Memont',
+                  f"""Подтвердите свой профиль, перейдя по ссылке\n 
+                  http://{current_host}/users/register/confirm/{code}/""",
+                  EMAIL_HOST_USER,
+                  [user.email])
+        return super().form_valid(form)
 
 
-class ProfileView(UpdateView):
+class RegisterMessageView(TemplateView):
+    template_name = 'users/register_message.html'
+
+
+class LoginRequiredTemplateView(TemplateView):
+    template_name = 'users/login_required.html'
+
+
+def confirm_register(code):
+    user = get_object_or_404(User, verification_code=code)
+    user.is_active = True
+    user.save()
+    return HttpResponseRedirect('/users/login')
+
+
+class UserPasswordRecoveryView(FormView):
+    form_class = UserPasswordResetForm
+    template_name = 'users/user_password_reset.html'
+    success_url = reverse_lazy('users:user_password_sent')
+
+    def form_valid(self, form):
+        email = form.cleaned_data['email']
+        user = User.objects.get(email=email)
+        characters = string.ascii_letters + string.digits
+        new_password = ''.join(random.choice(characters) for _ in range(10))
+        user.password = make_password(new_password)
+        user.save()
+        send_mail('Восстановление пароля на сайте Memont',
+                  f'Ваш новый пароль: {new_password}', EMAIL_HOST_USER,
+                  [user.email])
+        return super().form_valid(form)
+
+
+class UserPasswordSentView(TemplateView):
+    template_name = 'users/user_password_sent.html'
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     success_url = reverse_lazy('users:profile')
