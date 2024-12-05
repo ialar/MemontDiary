66b0807fa00b59fb8b3dccf6cd926daa5f43d478
Igor Larionov <jedialar@gmail.com>
2024-12-05 12:32:24 +0300

    cache enabled and add get_cache_for_entries_count

---
+++ diary/services.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.core.cache import cache
+
+from diary.models import Entry
+
+
+def get_cache_for_entries_count():
+    if settings.CACHE_ENABLED:
+        key = 'entries_count'
+        entries_count = cache.get(key)
+        if entries_count is None:
+            entries_count = Entry.objects.all().count()
+            cache.set(key, entries_count)
+    else:
+        entries_count = Entry.objects.all().count()
+    return entries_count
