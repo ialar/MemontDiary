35f0f24f8e47cd0e4d086bb4a8d0efea503c55b1
Igor Larionov <jedialar@gmail.com>
2024-12-10 00:00:32 +0300

    add comments for highlight filter

--- diary/templatetags/my_tags.py
+++ diary/templatetags/my_tags.py
@@ -16,9 +16,9 @@ def my_media_tag(data):
 def highlight(text, query):
     if query:
         # Добавляем <span class="highlight"> вокруг совпадений
-        highlighted_text = re.sub(
-            f"({re.escape(query)})",  # Экранируем запрос для безопасности
-            r'<span class="highlight">\1</span>',  # Оборачиваем совпадение
+        highlighted_text = re.sub(  # sub - функция, которая заменяет совпадения регулярного выражения в строке
+            f"({re.escape(query)})",  # Экранируем запрос для безопасности и игнорирования спецсимволов
+            r'<span class="highlight">\1</span>',  # Оборачиваем совпадение (\1 - первое захватывающее выражение)
             text,
             flags=re.IGNORECASE,  # Игнорируем регистр
         )
